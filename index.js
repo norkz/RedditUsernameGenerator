bee5e2dd2c71bf3ffc69360c57d45e28e89fb693
norkz <59128189+norkz@users.noreply.github.com>
2021-02-28 12:40:12 -0300

    idk

--- index.js
+++ index.js
@@ -5,14 +5,19 @@
 
 // If you're going to copy/change please give credits :D.
 
-const request = require('request'); // Import request package
+const request = require('request');
+const chalk = require('chalk');
+var api = 'https://www.reddit.com/api/v1/generate_username.json'; 
 
-{   // Start
-    var api = 'https://www.reddit.com/api/v1/generate_username.json'; // Set reddit.com API
-    request(api, function (error, response, body) { // API function
-        var parsedUsernames = JSON.parse(body); // Parse usernames from the api
-        console.log('code:', String(response && response.statusCode).replace(/200/g, '200 | RECEIVED').replace(/429/g, '429 | RATE-LIMIT') + '\n'); // print responses and rate-limits
-        console.log(String(parsedUsernames['usernames']).replace(/,/g, '\n').replace(/-/g, '_').replace(/undefined/g, 'Rate-Limit. Try again on 5 seconds.')) // Print usernames with spaces and without -
+{   
+    request(api, function (error, response, body) {  
+        var usernames = JSON.parse(body); 
+        console.log('code:', String(response && response.statusCode)
+                    .replace(/200/g, chalk.green('200 | RECEIVED'))
+                    .replace(/429/g, chalk.red('429 | RATE-LIMIT')) + chalk.bgGreen(chalk.black('\n------------------------------')));
+        console.log(String(usernames['usernames'])
+                    .replace(/,/g, '\n')
+                    .replace(/undefined/g, chalk.redBright('Rate-Limit. Try again on 5 seconds.')))
     })
-}   // End
+}  
 
